90663a6c790f50e2110e3b085fdd2d433e98803e
ThachVo <thach.vo@hubcba.com>
2017-08-17 14:33:00 +0700

    Remove uncheck method

--- app/src/main/java/cbsa/switchon/origination/common/fragment/FragmentUtils.java
+++ app/src/main/java/cbsa/switchon/origination/common/fragment/FragmentUtils.java
@@ -1,11 +1,10 @@
 package cbsa.switchon.origination.common.fragment;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 
 public class FragmentUtils {
-    public static <T extends Fragment> T getFragmentByClassTag(Class<T> clazz, FragmentManager fragmentManager) {
-        return (T) fragmentManager.findFragmentByTag(getDefaultFragmentTag(clazz));
+    private FragmentUtils() {
+        // Private cons
     }
 
     public static <T extends Fragment> String getDefaultFragmentTag(Class<T> clazz) {
