aa1e8b8415b04ca33c4f454c3b53fc0ac17e03d5
ThachVo <thach.vo@hubcba.com>
2017-08-17 13:50:46 +0700

    - Update crashlytic with latest vesion - Add keystore

--- app/src/main/java/cbsa/switchon/origination/application/MyApplication.java
+++ app/src/main/java/cbsa/switchon/origination/application/MyApplication.java
@@ -1,13 +1,19 @@
 package cbsa.switchon.origination.application;
 
 import android.app.Application;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.amb.retrofitwrapper.module.OkHttpModule;
+import com.amb.retrofitwrapper.module.RetrofitModule;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 
 import cbsa.switchon.origination.BuildConfig;
 import cbsa.switchon.origination.application.injection.AppComponent;
 import cbsa.switchon.origination.application.injection.DaggerAppComponent;
 import cbsa.switchon.origination.application.injection.modules.AppModule;
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 
@@ -20,26 +26,30 @@ public class MyApplication extends Application {
         super.onCreate();
         initializeApp();
         initLog();
-//        initCrashlytics();
+        initCrashlytics();
     }
 
-//    private void initCrashlytics() {
-//        CrashlyticsCore core = new CrashlyticsCore.Builder()
-//                .disabled(BuildConfig.DEBUG)
-//                .build();
-//        Fabric.with(this, new Crashlytics.Builder().core(core).build());
-//    }
+    private void initCrashlytics() {
+        // Set up Crashlytics, disabled for debug builds
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        // Initialize Fabric with the debug-disabled crashlytics.
+        Fabric.with(this, crashlyticsKit);
+    }
 
     private void initLog() {
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
-//        Timber.plant(new CrashlyticsTree());
+        Timber.plant(new CrashlyticsTree());
     }
 
     private void initializeApp() {
         appComponent = DaggerAppComponent.builder()
                 .appModule(new AppModule(this))
+                .retrofitModule(new RetrofitModule(ApiConstants.HOST_URL))
                 .okHttpModule(new OkHttpModule(ApiConstants.TIMEOUT_IN_MINUTE))
                 .build();
     }
@@ -48,26 +58,26 @@ public class MyApplication extends Application {
         return appComponent;
     }
 
-//    public class CrashlyticsTree extends Timber.Tree {
-//        private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
-//        private static final String CRASHLYTICS_KEY_TAG = "tag";
-//        private static final String CRASHLYTICS_KEY_MESSAGE = "message";
-//
-//        @Override
-//        protected void log(int priority, @Nullable String tag, @Nullable String message, @Nullable Throwable t) {
-//            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
-//                return;
-//            }
-//
-//            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
-//            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
-//            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
-//
-//            if (t == null) {
-//                Crashlytics.logException(new Exception(message));
-//            } else {
-//                Crashlytics.logException(t);
-//            }
-//        }
-//    }
+    private class CrashlyticsTree extends Timber.Tree {
+        private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
+        private static final String CRASHLYTICS_KEY_TAG = "tag";
+        private static final String CRASHLYTICS_KEY_MESSAGE = "message";
+
+        @Override
+        protected void log(int priority, @Nullable String tag, @Nullable String message, @Nullable Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+                return;
+            }
+
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
+
+            if (t == null) {
+                Crashlytics.logException(new Exception(message));
+            } else {
+                Crashlytics.logException(t);
+            }
+        }
+    }
 }
--- app/src/main/java/cbsa/switchon/origination/features/gettoken/GetTokenFragment.java
+++ app/src/main/java/cbsa/switchon/origination/features/gettoken/GetTokenFragment.java
@@ -1,12 +1,14 @@
 package cbsa.switchon.origination.features.gettoken;
 
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 
 import javax.inject.Inject;
 
 import cbsa.switchon.origination.R;
 import cbsa.switchon.origination.application.MyApplication;
+import cbsa.switchon.origination.common.activity.ContainerActivity;
 import cbsa.switchon.origination.common.fragment.BasePresenterFragment;
 import cbsa.switchon.origination.features.gettoken.injection.DaggerGetTokenComponent;
 import cbsa.switchon.origination.features.gettoken.injection.GetTokenContractor;
@@ -16,6 +18,12 @@ public class GetTokenFragment extends BasePresenterFragment<GetTokenContractor.G
 
     @Inject
     GetTokenContractor.GetTokenPresenter presenter;
+
+    public static void showMe(Activity activity) {
+        ContainerActivity.IntentBuilder intentBuilder = new ContainerActivity.IntentBuilder(activity)
+                .setFragmentClass(GetTokenFragment.class);
+        intentBuilder.start();
+    }
 
     @Override
     protected void onInitComponent() {
