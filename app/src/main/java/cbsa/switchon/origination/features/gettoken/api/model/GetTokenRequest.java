87118040007679b03a31c382481d666aec83083d
ThachVo <thach.vo@hubcba.com>
2017-10-05 13:14:14 +0700

    Remove test generate code

--- app/src/main/java/cbsa/switchon/origination/features/gettoken/api/model/GetTokenRequest.java
+++
@@ -1,8 +0,0 @@
-package cbsa.switchon.origination.features.gettoken.api.model;
-
-import cbsa.switchon.origination.application.api.model.BaseRequest;
-
-public class GetTokenRequest extends BaseRequest {
-
-
-}
--- app/src/main/java/cbsa/switchon/origination/features/gettoken/usecase/GetTokenUseCase.java
+++
@@ -1,9 +0,0 @@
-package cbsa.switchon.origination.features.gettoken.usecase;
-
-import cbsa.switchon.origination.features.gettoken.model.GetTokenResult;
-
-import io.reactivex.Observable;
-
-public interface GetTokenUseCase {
-    Observable<GetTokenResult> getGetToken();
-}
